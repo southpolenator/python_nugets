7745c3e2e50beab6105ce61c175d3bfb5e1bde6d
Taylor McKenna <tamr43@gmail.com>
2021-08-11 06:56:30 -0500

    Update PythonNuget.ps1

--- PythonNuget.ps1
+++ PythonNuget.ps1
@@ -8,14 +8,6 @@ param(
 # Find temp directory
 $temp = Resolve-Path -Path temp
 
-# Create 32bit nuget
-$extractedPath32 = "$temp\python32-full-$version"
-if (Test-Path -Path $extractedPath32)
-{
-    & "$PSScriptRoot\CreateNuget.ps1" -extractedPath $extractedPath32 -pythonVersion $version
-    Remove-Item -Path $extractedPath32 -Recurse
-}
-
 # Create 64bit nuget
 $extractedPath64 = "$temp\python64-full-$version"
 if (Test-Path -Path $extractedPath64)
